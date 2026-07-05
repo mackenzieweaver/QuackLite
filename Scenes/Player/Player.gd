f1e43f8d55346e783fcb708e10fac81e3868bbbd
Mackenzie Weaver <macknz7@gmail.com>
2026-07-05 15:30:54 -0400

    grounded movement

--- Scenes/Player/Player.gd
+++ Scenes/Player/Player.gd
@@ -4,15 +4,17 @@ const GROUP_NAME: String = "Player"
 
 
 @export var speed = 5.0
+@export_range(0.0, 0.5) var deceleration: float = 0.25
 @export var jump_speed = 15
 @export var sensitivity: float = 0.012
 @export var gravity: float = -30.0
 
 
-const CONCRETE_FOOTSTEPS_LOOP = preload("res://assets/Sounds/concrete-footsteps-6752.wav")
-const CONCRETE_FOOTSTEPS_STOP = preload("res://assets/Sounds/concrete-footsteps-stop.wav")
+# TODO play sounds appropriately
+const WALK_SOUND = preload("res://assets/Sounds/concrete-footsteps-6752.wav")
+const STOP_SOUND = preload("res://assets/Sounds/concrete-footsteps-stop.wav")
 const JUMP_SOUND = preload("res://assets/Sounds/667297_jump_05.wav")
-const _469567__PLAYER_DIE = preload("res://assets/Sounds/469567__PlayerDie.wav")
+const DEATH_SOUND = preload("res://assets/Sounds/469567__PlayerDie.wav")
 
 
 @onready var walking_sound: AudioStreamPlayer3D = $WalkingSound
@@ -59,20 +61,32 @@ func aim(_delta: float):
 func move_airborne(delta: float):
 	if not is_on_floor():
 		velocity.y += gravity * delta
+	elif Input.is_action_just_pressed("jump"):
+		velocity.y = jump_speed
 
 
 func move_grounded(_delta: float):
+	var input_dir: Vector2 = Input.get_vector("left", "right", "forward", "back")
+	var direction: Vector3 = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
+	if direction.length():
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, deceleration)
+		velocity.z = move_toward(velocity.z, 0, deceleration)
+	
 	move_and_slide()
 
 
 func play_footsteps() -> void:
-	walking_sound.stream = CONCRETE_FOOTSTEPS_LOOP
+	walking_sound.stream = WALK_SOUND
 	walking_sound.play()
 
 
 func play_stop_sound():
 	walking_sound.stop()
-	walking_sound.stream = CONCRETE_FOOTSTEPS_STOP
+	walking_sound.stream = STOP_SOUND
 	walking_sound.play()
 
 
