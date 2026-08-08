5f9e21f102e61bbc725d12f9cf8bf0eec07d6c40
Mackenzie Weaver <macknz7@gmail.com>
2026-08-08 14:14:15 -0400

    shoot grenade in correct direction and give the grenade inertia so that the torque spins it

--- Scenes/Weapons/Behaviors/ThrowableFire.gd
+++ Scenes/Weapons/Behaviors/ThrowableFire.gd
@@ -3,7 +3,7 @@ extends FireBehavior
 
 @export var throwable_scene: PackedScene
 @export var impulse: float = 15.0
-@export var torque := Vector3(0, 3, 0)
+@export var torque := Vector3(0, 0, 10)
 
 @export_flags_3d_physics var instant_explode_collision_mask: int = 0
 @export_flags_3d_physics var physics_collision_mask: int = 0
@@ -15,7 +15,17 @@ func fire(parent: Node3D, transform: Transform3D):
 	throwable.global_position = transform.origin
 	
 	# E.g. face the same way as the enemy
-	throwable.transform.basis = parent.transform.basis
+	throwable.transform.basis = transform.basis
 	
-	throwable.init(impulse, torque, damage, damage_collision_mask, instant_explode_collision_mask, physics_collision_mask)
+	throwable.inertia = Vector3.ONE
+	
+	throwable.init(
+		impulse,
+		torque,
+		damage,
+		damage_collision_mask,
+		instant_explode_collision_mask,
+		physics_collision_mask
+	)
+	
 	parent.get_tree().current_scene.add_child(throwable)
